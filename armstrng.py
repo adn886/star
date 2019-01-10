1e08cae2cd9b399e8f5007efd37e8262d40458be
adn886 <adshe889@gmail.com>
2019-01-11 01:25:48 +1200

    armstrng.py

---
+++ armstrng.py
@@ -0,0 +1,18 @@
+x = int(input(" Enter no "))
+p=x
+i = 0
+
+k = 0
+q = 0
+while(x != 0):
+    b = int(x % 10)
+
+    q = int(x/10)
+
+    k = k+pow(b, 3)
+    print(k)
+    x = q
+if k == p:
+    print(" NO u enterd is armstrong ")
+else:
+    print(" no is !not armstrong ")
